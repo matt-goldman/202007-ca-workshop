fe7b93a52ba94e46dcc32ce7c1d9d51e8863dac3
Matt Goldman <mattgoldman1981@gmail.com>
2020-07-09 14:13:37 +1000

    fix broken dependencies

--- CaWorkshop/CaWorkshop.WebUI/Startup.cs
+++ CaWorkshop/CaWorkshop.WebUI/Startup.cs
@@ -1,13 +1,11 @@
-using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.SpaServices.AngularCli;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using CaWorkshop.Infrastructure.Identity;
-using CaWorkshop.Infrastructure.Persistence;
+using CaWorkshop.Application;
+using CaWorkshop.Infrastructure;
 
 namespace CaWorkshop.WebUI
 {
@@ -23,18 +21,9 @@ namespace CaWorkshop.WebUI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ApplicationDbContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
+            services.AddInfrastructure(Configuration);
+            services.AddApplication(Configuration);
 
-            services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
-                .AddEntityFrameworkStores<ApplicationDbContext>();
-
-            services.AddIdentityServer()
-                .AddApiAuthorization<ApplicationUser, ApplicationDbContext>();
-
-            services.AddAuthentication()
-                .AddIdentityServerJwt();
             services.AddControllersWithViews();
             services.AddRazorPages();
 
